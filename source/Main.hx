5cbfc87a8851b3dcec71837785f5512873e437a8
Yeonho Jang <siabard@gmail.com>
2019-09-25 08:29:29 +0900

    initial commit

---
+++ source/Main.hx
@@ -0,0 +1,11 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+class Main extends Sprite {
+	public function new() {
+		super();
+		addChild(new FlxGame(320, 180, PlayState));
+	}
+}
---
+++ source/PlayState.hx
@@ -0,0 +1,45 @@
+package;
+
+import flixel.FlxG;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+import flixel.tile.FlxTilemap;
+import flixel.FlxState;
+
+class PlayState extends FlxState {
+	var map:FlxTilemap;
+	var player:FlxSprite;
+	var mapData:Array<Int> = [
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	];
+
+	override public function create():Void {
+		map = new FlxTilemap();
+		map.loadMapFromArray(mapData, 20, 12, AssetPaths.tiles__png, 16, 16);
+		add(map);
+
+		player = new FlxSprite(64, 0);
+		player.makeGraphic(16, 16, FlxColor.RED);
+		player.acceleration.y = 420;
+		add(player);
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void {
+		super.update(elapsed);
+
+		FlxG.collide(map, player);
+	}
+}
